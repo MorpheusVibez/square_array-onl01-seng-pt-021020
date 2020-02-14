45fbfd530e44f771d608ae7cbf4f9899b5141feb
Andre Skeene <halodre24@gmail.com>
2020-02-14 03:30:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
   new_array = []
   count = 0 
-  
-  array.each do |num|
+ 
+  array.each do 
    new_array << array[count] ** 2
     count += 1
-    
   end
   new_array
 end
